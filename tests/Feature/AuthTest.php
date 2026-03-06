9c85ace93bf34e4c6a64443d8021cf65491b82bf
Dre <124632408+DreGi0@users.noreply.github.com>
2026-03-05 22:23:45 -0600

    feat: agregado pruebas de la función de autenticación y configurar el entorno de pruebas

--- tests/Feature/AuthTest.php
+++ tests/Feature/AuthTest.php
@@ -33,4 +33,71 @@ class AuthTest extends TestCase
 
         $this->assertAuthenticatedAs($user);
     }
+
+    public function test_login_with_invalid_credentials() 
+    {
+        $response = $this->postJson('/api/v1/login', [
+            'email' => 'noexistes@example.com',
+            'password' => 'passincorrecta',
+        ]);
+
+        $response->assertStatus(422)
+            ->assertJson(['message' => 'Invalid credentials']);
+    }
+
+    public function test_user_can_logout()
+    {
+        $user = User::factory()->create([
+            'password' => bcrypt('test123')
+        ]);
+
+        $loginResponse = $this->postJson('/api/v1/login', [
+            'email'    => $user->email,
+            'password' => 'test123',
+        ]);
+
+        $token = $loginResponse->json('access_token');
+
+        $response = $this->withToken($token)
+            ->postJson('/api/v1/logout');
+
+        $response->assertStatus(200)
+            ->assertJson(['message' => 'Logged out successfully']);
+    }
+
+    public function test_logout_fails_without_auth()
+    {
+        $response = $this->postJson('/api/v1/logout');
+
+        $response->assertStatus(401);
+    }
+
+    public function test_user_can_see_profile()
+    {
+        $user = User::factory()->create([
+            'password' => bcrypt('test123')
+        ]);
+
+        $loginResponse = $this->postJson('/api/v1/login', [
+            'email'    => $user->email,
+            'password' => 'test123',
+        ]);
+
+        $token = $loginResponse->json('access_token');
+
+        $response = $this->withToken($token)
+            ->getJson('/api/v1/profile');
+
+        $response->assertStatus(200)
+            ->assertJsonPath('user.email', $user->email);
+    }
+
+    public function test_profile_fails_without_auth()
+    {
+        $response = $this->getJson('/api/v1/profile');
+
+        $response->assertStatus(401);
+    }
+
+    
 }
